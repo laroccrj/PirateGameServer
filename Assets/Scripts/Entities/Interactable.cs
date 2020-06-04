a7298a1661976ddc7020b869f76f89b884610b54
Rob La Rocca <laroccrj@gmail.com>
2020-06-04 00:27:38 -0400

    Moved away from mounter logic in favor of interactable logic

---
+++ Assets/Scripts/Entities/Interactable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public interface Interactable
+{   
+    // The spot the pirate needs to reach to interact in world space
+    Vector3 GetInteractionPoint();
+
+    void Interact(Pirate pirate);
+
+    void Leave();
+
+    Transform GetSeat();
+}
